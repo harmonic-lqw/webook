e8b86e5ef11b9b293c435973ec1c46c2b512059f
harmonic-lqw <923377580@qq.com>
2023-11-28 13:18:10 +0800

    docker compose test

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"strings"
 	"time"
 	"webook/internal/web"
 )
@@ -12,15 +11,12 @@ func main() {
 	hdl := web.NewUserHandler()
 	server := gin.Default()
 	server.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
-		AllowMethods:     []string{"GET", "POST"},
-		AllowHeaders:     []string{"Content-Type"},
+		//AllowAllOrigins: true,
+		//AllowOrigins:     []string{"http://localhost:3000"},
 		AllowCredentials: true,
+		AllowHeaders:     []string{"authorization", "content-type"},
 		AllowOriginFunc: func(origin string) bool {
-			if strings.Contains(origin, "http://localhost") {
-				return true
-			}
-			return strings.Contains(origin, "your_company.com")
+			return true
 		},
 		MaxAge: 12 * time.Hour,
 	}))
